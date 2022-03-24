abdca2231a82bcd735bb4e41ab0f167e7234dc8d
riskezwn <enriquegarciagasc@gmail.com>
2022-03-24 11:36:42 +0100

    Add event custom style

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -19,6 +19,18 @@ const events = [
 ];
 
 export const CalendarScreen = () => {
+  const eventStyleGenerator = (event, start, end, isSelected) => { 
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white'
+    }
+    return {
+      style
+    }
+   }
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -29,6 +41,7 @@ export const CalendarScreen = () => {
         startAccessor="start"
         endAccessor="end"
         messages={messages}
+        eventPropGetter={eventStyleGenerator}
       />
     </div>
   );
