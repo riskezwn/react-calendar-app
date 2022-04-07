682d05ffa7e186039da5568460cfabb82412b0e7
riskezwn <enriquegarciagasc@gmail.com>
2022-04-07 14:30:39 +0200

    create new user

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,10 @@
+import Swal from "sweetalert2";
 import { fetchWithoutToken } from "../helpers/fetch";
 import { types } from "../types/types";
 
 export const startLogin = (email, password) => {
   return async (dispatch) => {
-    const resp = await fetchWithoutToken("auth/", { email, password }, "POST");
+    const resp = await fetchWithoutToken("auth", { email, password }, "POST");
     const body = await resp.json();
 
     if (body.ok) {
@@ -16,6 +17,33 @@ export const startLogin = (email, password) => {
           name: body.name,
         })
       );
+    } else {
+      Swal.fire("Error", body.msg, "error");
+    }
+  };
+};
+
+export const startRegister = (name, email, password) => {
+  return async (dispatch) => {
+    const resp = await fetchWithoutToken(
+      "auth/new",
+      { name, email, password },
+      "POST"
+    );
+    const body = await resp.json();
+
+    if (body.ok) {
+      localStorage.setItem("token", body.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+
+      dispatch(
+        login({
+          uid: body.uid,
+          name: body.name,
+        })
+      );
+    } else {
+      Swal.fire("Error", body.msg, "error");
     }
   };
 };
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useForm } from "../../hooks/useForm";
-import { startLogin } from "../../actions/auth";
+import { startLogin, startRegister } from "../../actions/auth";
 import "./css/login.css";
+import Swal from "sweetalert2";
 
 export const LoginScreen = () => {
   const dispatch = useDispatch();
@@ -11,12 +12,29 @@ export const LoginScreen = () => {
     lEmail: "riskezwn@gmail.com",
     lPassword: "123456",
   });
+  const [formRegisterValues, handleRegisterInputChange] = useForm({
+    rName: "Enrique",
+    rEmail: "enriquegarciagasc@gmail.com",
+    rPassword1: "123456",
+    rPassword2: "123456",
+  });
 
   const { lEmail, lPassword } = formLoginValues;
+  const { rName, rEmail, rPassword1, rPassword2 } = formRegisterValues;
 
   const handleLogin = (e) => {
     e.preventDefault();
     dispatch(startLogin(lEmail, lPassword));
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    if (rPassword1 !== rPassword2) {
+      return Swal.fire("Error", "Las contraseñas deben coincidir", "error");
+    }
+
+    dispatch(startRegister(rName, rEmail, rPassword1));
   };
 
   return (
@@ -55,12 +73,15 @@ export const LoginScreen = () => {
 
             <div className="col-md-6 login-form-2">
               <h3>Registro</h3>
-              <form>
+              <form onSubmit={handleRegister}>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control"
                     placeholder="Nombre"
+                    name="rName"
+                    value={rName}
+                    onChange={handleRegisterInputChange}
                   />
                 </div>
                 <div className="form-group">
@@ -68,6 +89,9 @@ export const LoginScreen = () => {
                     type="email"
                     className="form-control"
                     placeholder="Correo"
+                    name="rEmail"
+                    value={rEmail}
+                    onChange={handleRegisterInputChange}
                   />
                 </div>
                 <div className="form-group">
@@ -75,6 +99,9 @@ export const LoginScreen = () => {
                     type="password"
                     className="form-control"
                     placeholder="Contraseña"
+                    name="rPassword1"
+                    value={rPassword1}
+                    onChange={handleRegisterInputChange}
                   />
                 </div>
 
@@ -83,6 +110,9 @@ export const LoginScreen = () => {
                     type="password"
                     className="form-control"
                     placeholder="Repita la contraseña"
+                    name="rPassword2"
+                    value={rPassword2}
+                    onChange={handleRegisterInputChange}
                   />
                 </div>
 
