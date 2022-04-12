8d3343cc2b3f2d24764c9becd95fd933234d25a6
riskezwn <enriquegarciagasc@gmail.com>
2022-04-12 12:48:30 +0200

    add logout

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { startLogout } from '../../actions/auth';
 
 export const Navbar = () => {
+  const dispatch = useDispatch();
+  const { name } = useSelector((store) => store.auth);
+
+  const handleLogout = () => {
+    dispatch(startLogout());
+  }
+
   return (
     <nav className='navbar navbar-dark bg-dark p-2 h-30'>
-        <span className='navbar-brand'>Riskezwn</span>
-        <button className='btn btn-outline-danger'>
-            <span><i className='fas fa-sign-out-alt'></i> Salir</span>
-        </button>
+      <span className='navbar-brand'>{name}</span>
+      <button
+        className='btn btn-outline-danger'
+        onClick={handleLogout}
+      >
+        <span><i className='fas fa-sign-out-alt'></i> Salir</span>
+      </button>
     </nav>
   )
 }
